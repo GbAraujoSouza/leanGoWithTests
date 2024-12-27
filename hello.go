854083967461072392fb0973ef3813aa376ca7ed
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-12-27 19:00:43 -0300

    refactor: final hello world logic with different languages

--- hello.go
+++ hello.go
@@ -1,16 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const englishHelloPrefix = "Hello, meu faixa ";
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string) string {
-  if (name == "") {
-    name = "World"
-  }
-	return englishHelloPrefix + name;
+	englishPrefix = "Hello, meu faixa "
+	spanishPrefix = "Hola, meu cria "
+	frenchPrefix  = "Bonjour, meu cria "
+)
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishPrefix
+	case french:
+		prefix = frenchPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
 }
 
 func main() {
-  fmt.Println(Hello("Gabriel"));
+	fmt.Println(Hello("Gabriel", ""))
 }
