90003c56579412f59e531378226daa28042204df
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-12-29 18:43:40 -0300

    feat: slices with tests

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -7,3 +7,23 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sum []int
+	for _, numbers := range numbersToSum {
+		sum = append(sum, Sum(numbers))
+	}
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sum []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sum = append(sum, 0)
+		} else {
+			sum = append(sum, Sum(numbers[1:]))
+		}
+	}
+	return sum
+}
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("colection of any size", func(t *testing.T) {
@@ -11,5 +14,36 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d, give %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("sum of each slice n of a set of n slices", func(t *testing.T) {
+		var got []int = SumAll([]int{1, 2}, []int{1, 2, 3})
+		var want []int = []int{3, 6}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(slice1, slice2 []int, t *testing.T) {
+		if !reflect.DeepEqual(slice1, slice2) {
+			t.Errorf("got %v want %v", slice1, slice2)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{1, 9, 1})
+		want := []int{2, 10}
+		checkSums(got, want, t)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 9, 1})
+		want := []int{0, 10}
+		checkSums(got, want, t)
 	})
 }
